f59923ddeb3273a84a57f8f1aeffc9995e4931bb
Aline Ibaldo Gonçalves <aline_ibaldo@hotmail.com>
2025-07-25 23:54:46 -0300

    com videos

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+  projectId: 'mgwubm',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implementar ouvintes de eventos do Node aqui, se necessário
     },
+    video: true, // Habilitar gravação de vídeos
+    videoCompression: 32, // Compressão de vídeo (opcional)
+    videoUploadOnPasses: false, // Não enviar vídeos quando o teste passar
+    screenshotOnRunFailure: true, // Habilita screenshots quando o teste falhar
+    screenshotsFolder: 'cypress/screenshots', // Defina a pasta onde as screenshots serão salvas
   },
 };
+
